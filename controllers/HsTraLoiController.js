8f93d3d749d94249f1c3ceccc6cac345df9eeb6d
Ha Van Thanh <thanhhv3@vng.com.vn>
2020-11-23 05:07:36 +0700

    cap nhat chuc nang, them, sua xoa cho HsTraLoiController

--- controllers/HsTraLoiController.js
+++ controllers/HsTraLoiController.js
@@ -5,6 +5,7 @@ const Joi = require('joi');
 const { BadRequest, Created } = require('../utils/httpResponse');
 const stringUtil = require('../utils/stringUtil');
 const { Op } = require('sequelize');
+const modelName = 'TaHsTraLoi'
 
 const requestHandler = new RequestHandler(new Logger());
 
@@ -12,22 +13,70 @@ class HsTraLoiController extends BaseController {
   static async addCauTraLoi(req, res) {
     try {
       const schema = Joi.object({
-        TenNganHang: Joi.string().max(100).required(),
-        LinhVuc: Joi.string().required(),
-        MoTa: Joi.string(),
-        ThuocTinh: Joi.number().integer()
+        MaBaiThi: Joi.string().max(100).required(),
+        MaUserThi: Joi.string().required(),
+        DiemThi: Joi.number().integer(),
+        MaCauHoi: Joi.number().integer(),
+        TraLoi: Joi.number().integer()
       });
 
       const { error } = schema.validate(req.body);
-      requestHandler.validateJoi(error, BadRequest.status, BadRequest.error, 'Data is invalid');
+      requestHandler.validateJoi(error, BadRequest.status, BadRequest.error, 'Invalid HsTraLoi addCauTraLoi data');
 
-      req.body.Id = `nganhang_${stringUtil.generateString()}`;
-      const result = await super.create(req, 'TaNganHang');
+      req.body.Id = `hstraloi_${stringUtil.generateString()}`;
+      const result = await super.create(req, 'TaHsTraLoi');
 
-      requestHandler.sendSuccess(res, 'NganHang is created successfully', Created.status)(result);
+      requestHandler.sendSuccess(res, 'HsTraLoi is created successfully', Created.status)(result.dataValues);
     } catch (error) {
       requestHandler.sendError(req, res, error);
     }
+  }
+
+  static async modifyHsTraLoi(req, res) {
+    try {
+      const schema = Joi.object({
+        DiemThi: Joi.number().integer.required(),
+        TraLoi: Joi.number().integer().required(),
+      });
+      const { error } = schema.validate(req.body);
+      requestHandler.validateJoi(error, BadRequest.status, BadRequest.error, 'invalid HsTraLoi');
+
+      const result = await super.updateById(req, modelName, req.body);
+      return requestHandler.sendSuccess(res, 'HsTraLoi is modified successfully', OK.status)(result);
+    } catch (error) {
+      return requestHandler.sendError(req, res, error);
+    }
+  }
+
+  static async deleteByMaCauTraLoi(req, res) {
+    try {
+      const result = await super.deleteById(req, modelName);
+      return requestHandler.sendSuccess(res, 'HsTraLoi Deleted Successfully')(result);
+    } catch (err) {
+      return requestHandler.sendError(req, res, err);
+    }
+  }
+
+  static async getDapAnHsTraLoi(req, res) {
+    try {
+      const schema = Joi.object({
+          MaCauTraLoi: Joi.string().max(50) ,
+      });
+
+      const { error } = schema.validate(req.params);
+      requestHandler.validateJoi(error, BadRequest.status, BadRequest.error, 'invalid');
+
+      const options = {}
+      options.where = {
+          MaCauTraLoi: req.params.MaCauTraLoi
+      }
+
+      const rep = await super.getByCustomOptions(req, modelName, options);
+      requestHandler.sendSuccess(res, 'Dap an: ')(rep);
+
+  } catch (error) {
+      requestHandler.sendError(req, res, error);
+  }
   }
 
   static async listCauTraLoi(req, res) {
--- router/api/hsTraLoiRouter.js
+++ router/api/hsTraLoiRouter.js
@@ -6,22 +6,23 @@ const HsTraLoiController = require('../../controllers/HsTraLoiController');
  * @swagger
  * definitions:
  *  TaHsTraLoi:
- *    required:
- *      - MaCauTraLoi
+ *    required:   
  *      - MaBaiThi
  *      - MaUserThi
+ *      - DiemThi
  *      - MaCauHoi
+ *      - TraLoi
  *      
  *    properties:
- *      MaCauTraLoi:
- *        type: string
- *        description: macautraloi_`random string length 100`
  *      MaBaiThi:
  *        type: string
+ *        description: baithi_`random string length 10`
  *      MaUserThi:
  *        type: string
+ *        description: user_`random string length 10`
  *      DiemThi:
  *        type: float
+ *        description: diemthi_`number in 0-10`
  *      MaCauHoi: 
  *        type: string
  *      TraLoi:
@@ -42,23 +43,19 @@ const HsTraLoiController = require('../../controllers/HsTraLoiController');
  *    parameters:
  *      - name: body
  *        in: body
+ *        description: data of HsTraLoi  
  *        required: true
  *        schema:
  *          type: object
- *          required:
- *            - MaCauTraLoi
- *            - MaBaiThi
- *            - MaUserThi 
- *            
+ *          required: 
+ *              - MaCauTraLoi
  *          properties:
- *            MaCauTraLoi:
- *              type: string
  *            MaBaiThi:
  *              type: string
  *            MaUserThi:
  *              type: string
  *            DiemThi:
- *              type: integer
+ *              type: float
  *            MaCauHoi:
  *              type: string
  *            TraLoi:
@@ -72,11 +69,45 @@ router.post('/add', auth.isAuthunticated, HsTraLoiController.addCauTraLoi);
 
 /**
  * @swagger
+ * /hstraloi/mod/{MaCauTraLoi}:
+ *  put:
+ *    tags:
+ *      - HsTraLoi
+ *    produces:
+ *      - application/json
+ *    security:
+ *      - Bearer: []
+ *    parameters:
+ *      - name: MaCauTraLoi
+ *        in: path
+ *        required: true
+ *        type: string  
+ *      - name: body
+ *        in: body
+ *        description: modify your cau tra loi cua hoc sinh
+ *        required: true
+ *        schema:
+ *          type: object
+ *          properties:
+ *            DiemThi:
+ *              type: int
+ *              description: kieu nguyen
+ *            TraLoi:
+ *              type: int
+ *    responses:
+ *      200:
+ *        description: HsTraLoi has been modified successfully
+ */
+
+router.put('/mod', auth.isAuthunticated, HsTraLoiController.modifyHsTraLoi);
+
+/**
+ * @swagger
  * /hstraloi/list:
  *  get:
  *    tags: 
  *      - HsTraLoi
- *    description: HsTraLoi APIs
+ *    description: Hien hi diem thi, cau tra loi cua cau hoi do
  *    security: 
  *      - Bearer: []
  *    produces:
@@ -100,12 +131,58 @@ router.post('/add', auth.isAuthunticated, HsTraLoiController.addCauTraLoi);
  *        type: int
  *      - name: TraLoi
  *        in: query
- *        type: string 
+ *        type: int
  *    responses:
  *      200: 
  *        description: List HSTraLoi OK
  */
 
 router.get('/list', auth.isAuthunticated, HsTraLoiController.listCauTraLoi);
+
+/**
+ * @swagger
+ * /hstraloi/{MaCauTraLoi}:
+ *   delete:
+ *     tags:
+ *       - HsTraLoi
+ *     security:
+ *       - Bearer: []
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: MaCauTraLoi
+ *        description: MaCauTraLoi muon xoa
+ *        in: path
+ *        required: true
+ *        type: string
+ *     responses:
+ *       200:
+ *         description: delete HsTraLoi by MaCauTraLoi
+ */
+router.delete('/:Id(user_[a-zA-Z0-9]{10})', auth.isAuthunticated, HsTraLoiController.deleteByMaCauTraLoi);
+
+
+/**
+ * @swagger
+ * /hstraloi/getDapAnHsTraLoi/{MaCauTraLoi}:
+ *  get:
+ *    tags: 
+ *      - HsTraLoi
+ *    description: DapAn APIs
+ *    security: 
+ *      - Bearer: []
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: MaCauTraLoi
+ *        in: path
+ *        required: true
+ *        type: string
+ *    responses:
+ *      200: 
+ *        description: Dap an ok
+ */
+
+router.get('/getDapAnHsTraLoi/:MaCauTraLoi', auth.isAuthunticated, HsTraLoiController.getDapAnHsTraLoi);
 
 module.exports = router;
